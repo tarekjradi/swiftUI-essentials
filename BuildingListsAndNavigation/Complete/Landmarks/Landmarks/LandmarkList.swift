8eb2b34f6710107a8d30045bcdd80fd6566d89ea
Tarek Abdala <tarek.jradi@altran.com>
2020-01-29 17:36:36 +0000

    changing the list for edit mode

--- BuildingListsAndNavigation/Complete/Landmarks/Landmarks/LandmarkList.swift
+++ BuildingListsAndNavigation/Complete/Landmarks/Landmarks/LandmarkList.swift
@@ -8,21 +8,31 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var isEditMode: EditMode = .inactive
+
     var body: some View {
         NavigationView {
             List(landmarkData) { landmark in
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
                 }
+                if (self.isEditMode == .active) {
+                    Text("now is edit mode")
+                } else  {
+                    Text("now is no edit mode")
+                }
+
             }
+            .navigationBarItems(trailing: EditButton())
             .navigationBarTitle(Text("Landmarks"))
+            .environment(\.editMode, self.$isEditMode)
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+        ForEach(["iPhone SE", "iPad Air 2"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
